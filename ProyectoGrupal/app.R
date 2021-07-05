d4e5e356d0bc6cda5f51bd639d3954835278c0af
LuisGagn <luisgagnevin@gmail.com>
2021-07-05 02:08:41 -0300

    Uso de Giraffe

--- ProyectoGrupal/app.R
+++ ProyectoGrupal/app.R
@@ -8,6 +8,7 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(ggplot2)
 library(lubridate)
+library(ggiraph)
 
 # DataSets || En caso de no descargar COVID.csv, comentar la fila 14 y descomentar la 13
 #cgd<-read.csv("https://query.data.world/s/25w3mrdsnje6zupnls5pajt3ruf6in", header=TRUE, stringsAsFactors=FALSE);
@@ -21,13 +22,15 @@ ui <- fluidPage(
   tabsetPanel(
     tabPanel("Global",h1("Casos Globales"),
              sidebarLayout(
-               sidebarPanel(dateInput("fecha", "Selecciona la Fecha",min = "2020-01-21", max="2021-07-01", value="2020-02-20")),mainPanel(
-                 plotOutput("mapa"),DT::dataTableOutput("tabla")
+               sidebarPanel(dateInput("fecha", "Selecciona la Fecha",min = "2020-01-21", max="2021-07-01", value="2020-02-20"),
+                            ),mainPanel(
+                              girafeOutput("mapa"),DT::dataTableOutput("tabla")
                ))),    
     
-    tabPanel("America",h1("Test"),sidebarLayout(
-               sidebarPanel(dateInput("fecha", "Selecciona la Fecha",min = "2020-01-21", max="2021-07-01", value="2020-02-20")),mainPanel(
-                 
+    tabPanel("Vacunacion",h1("Test"),sidebarLayout(
+               sidebarPanel(),mainPanel(
+                 girafeOutput("vacunas", height = 700, width = 700)
+
                )))
 ))
 
@@ -62,19 +65,41 @@ server <- function(input, output, session) {
     world <- ne_countries(scale = "medium", returnclass = "sf")
     COVID.world <- reactive(merge(world, casos(), by.x="admin", by.y="COUNTRY_SHORT_NAME"))
   
-# Mapamundi Grafico
+    
+ 
+    
+    
+    
+# Mapamundi Grafico Paises
     graf<-reactive(ggplot(COVID.world()) + 
-                   geom_sf(aes(fill=Casos)) +
+                   geom_sf_interactive(aes(tooltip=Casos,fill=Casos),color = NA) +
                    scale_fill_gradient(label=comma) +
                    theme_classic()
                    )
-  
-  
-    
-#Calls
-  output$mapa<-renderPlot(graf())
-  output$tabla <- DT::renderDataTable({casos()})
 
+####
+
+####    
+    
+    
+    
+    
+# Vacunacion    
+    vacunacion$date<- as.Date(vacunacion$date)
+   
+   a<-reactive(a<- ggplot(vacunacion,aes(x=date,y=total_vaccinations))+
+     geom_point_interactive(aes(tooltip=date, data_id=date),colour="darkgreen", shape=19)+
+      scale_y_continuous(labels=comma)+scale_x_date(date_breaks = "15 days")+
+      geom_line(colour="red")+
+      labs(y="Vacunas Totales", x="Fecha")
+   )
+      
+      
+   
+#Calls
+  output$mapa<-renderGirafe(girafe(ggobj = graf(),width_svg = 7, height_svg = 4, options=list(opts_sizing(rescale=TRUE))))
+  output$tabla <- DT::renderDataTable({casos()})
+  output$vacunas <-  renderGirafe(girafe(ggobj = a()))
 }
 
 
